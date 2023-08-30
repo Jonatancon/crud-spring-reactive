1c48ca58a7a66e5b52d60d633f2e61310955d493
Jonatancon <jhonatanlora2011@gmail.com>
2023-08-30 16:48:39 -0500

    feat (Base project):
    Implement the base project with spring reactor and clean architecture, for now only implement connections to MySqlDB in static code possible in a future with dynamic parameter from AWS secret manager.

---
+++ src/main/java/com/reactive/webflux/CrudSpringReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringReactiveApplication.class, args);
+	}
+
+}
